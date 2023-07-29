3e043398a41f39b33652db037e6c0a10fac4ed51
Philippe <55062877+Philippe35507@users.noreply.github.com>
2023-07-29 19:26:24 +0100

    Quick fixes Nav Styling

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,11 +3,11 @@ import ThemeButton from "./ThemeButton";
 
 export default function Navbar() {
   return (
-    <div className="max-w-3xl mx-auto px-4 sm:px6 lg:px-8">
+    <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
       <div className="flex justify-between h-16">
         <div className="flex justify-between items-center w-full">
           <Link href="/">
-            <h1 className="text 2xl font-medium">
+            <h1 className="text-2xl font-medium">
               Jan <span className="text-teal-500">Blog</span>
             </h1>
           </Link>
