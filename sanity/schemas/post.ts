2c006ac36115fd738d2411efede3e31af767ce1f
Philippe <55062877+Philippe35507@users.noreply.github.com>
2023-07-29 20:31:00 +0100

    Creating a schema
    needs document type

---
+++ sanity/schemas/post.ts
@@ -0,0 +1,19 @@
+export default {
+  name: 'post',
+  type: 'document',
+  title: 'Post',
+  fields: [
+    {name: 'title', type: 'string', title: 'Title'},
+    {name: 'overview', type: 'string', title: 'Overview'},
+    {name: 'slug', type: 'slug', title: 'Slug'},
+    {
+      name: 'content',
+      type: 'array',
+      title: 'Content',
+      of: [
+        {type: 'block'},
+        {type: 'image', fields: [{type: 'text', name: 'alt', title: 'Alternative Text'}]},
+      ],
+    },
+  ],
+}
