42a6ce30863ed3332788f9ae8f14bd71b5a0aa0c
Philippe <55062877+Philippe35507@users.noreply.github.com>
2023-07-29 23:37:57 +0100

    Fetching data

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
-export default function IndexPage() {
+import { client } from "./lib/sanity";
+
+async function getData() {
+  const query = `*[_type == "post]`;
+
+  const data = await client.fetch(query);
+
+  return data;
+}
+
+export default async function IndexPage() {
+  const data = await getData();
   return <h1>Hello from the Index Page</h1>;
 }
