29397698042274a0f5b43b2dab0c9b5fdf49f0fb
Philippe <55062877+Philippe35507@users.noreply.github.com>
2023-07-29 19:25:15 +0100

    Rendering the right icon

--- app/components/ThemeButton.tsx
+++ app/components/ThemeButton.tsx
@@ -1,8 +1,18 @@
 "use client";
+
 import { useTheme } from "next-themes";
+import { useState, useEffect } from "react";
 
 export default function ThemeButton() {
   const { setTheme, resolvedTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) {
+    return null;
+  }
 
   return (
     <button
